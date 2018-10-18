8054fb6b1b7c34ac8b1f60f41793ce42c9ee01ff
konyshevn <41689868+konyshevn@users.noreply.github.com>
2018-10-18 11:32:53 +0300

    Форма modelformset_factory

---
+++ inv/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.forms import ModelForm, modelformset_factory
+from inv.models import *
+
+
+class DocIncomeForm(ModelForm):
+    class Meta:
+        model = DocIncome
+        fields = ['doc_date', 'doc_num', 'department', 'stock']
+
+
+DocIncomeTableUnitFormSetBase = modelformset_factory(DocIncomeTableUnit, form=DocIncomeForm, fields=['device', 'person', 'comment'], can_delete=True)
+
+
+
+#class DocIncomeTableUnitFormSet(DocIncomeTableUnitFormSetBase):
