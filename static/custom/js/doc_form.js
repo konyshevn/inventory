9319af9bf515abb4d63e2be5a0e12f69c6d3982b
konyshevn <41689868+konyshevn@users.noreply.github.com>
2018-12-13 16:59:07 +0300

    fix add_row & my_selectize.js - selectize_widget_init

--- static/custom/js/doc_form.js
+++ static/custom/js/doc_form.js
@@ -9,81 +9,11 @@ function add_row() {
 
     var cloned = tr.clone();  
     tr.find('.selectize_widget').each(function(){
-        var field_id = $(this).data("field_id")
-        $(this).selectize({
-            create: false,
-            sortField: 'text',
-            valueField: 'value',
-            labelField: 'text',
-            searchField: ['text'],
-            openOnFocus: true,
-            //  closeAfterSelect: true,
-
-            render: {
-                option: function (item, escape) {
-                    return '<div>' + escape(item.text) + '</div>';
-                }
-            },
-
-            load: function(query, callback) {
-                if (!query.length) return callback();
-                //url = $("#myselectize").data('url');
-                $.ajax({
-                    url: '/selectize_ajax_query/',
-                    data: { 'q': query, 'field_id': field_id },
-                    dataType: "json",
-                    type: 'GET',
-
-                    error: function() {
-                        console.log(res);
-                        callback();
-                    },
-                    success: function(res) {
-                        console.log(res);
-                        callback(res);
-                    }
-                })
-            },
-        });   
+        selectize_widget_init($(this))
     })
 
     cloned.find('.selectize_widget').each(function(){
-        var field_id = $(this).data("field_id")
-        $(this).selectize({
-            create: false,
-            sortField: 'text',
-            valueField: 'value',
-            labelField: 'text',
-            searchField: ['text'],
-            openOnFocus: true,
-            //  closeAfterSelect: true,
-
-            render: {
-                option: function (item, escape) {
-                    return '<div>' + escape(item.text) + '</div>';
-                }
-            },
-
-            load: function(query, callback) {
-                if (!query.length) return callback();
-                //url = $("#myselectize").data('url');
-                $.ajax({
-                    url: '/selectize_ajax_query/',
-                    data: { 'q': query, 'field_id': field_id },
-                    dataType: "json",
-                    type: 'GET',
-
-                    error: function() {
-                        console.log(res);
-                        callback();
-                    },
-                    success: function(res) {
-                        console.log(res);
-                        callback(res);
-                    }
-                })
-            },
-        });   
+        selectize_widget_init($(this))
     })
 
     //tr.find('selectize').removeAttr('data-select2-id').removeAttr('id').select2();
--- static/custom/js/my_selectize.js
+++ static/custom/js/my_selectize.js
@@ -1,53 +1,49 @@
+function selectize_widget_init(item) {
+    var field_id = item.data("field_id")
+    //console.log(field_id);
+    var $select = item.selectize({
+        create: false,
+        sortField: 'text',
+        valueField: 'value',
+        labelField: 'text',
+        searchField: ['text'],
+        openOnFocus: true,
+        //  closeAfterSelect: true,
+
+        render: {
+            option: function (item, escape) {
+                return '<div>' + escape(item.text) + '</div>';
+            }
+        },
+
+        load: function(query, callback) {
+            if (!query.length) return callback();
+            //url = $("#myselectize").data('url');
+            $.ajax({
+                url: '/selectize_ajax_query/',
+                data: { 'q': query, 'field_id': field_id },
+                dataType: "json",
+                type: 'GET',
+
+                error: function() {
+                    console.log(res);
+                    callback();
+                },
+                success: function(res) {
+                    console.log(res);
+                    callback(res);
+                }
+            })
+        },
+    });
+    $select[0].selectize.on( 'dropdown_close', function () {
+        $select[0].selectize.clearOptions();
+    });
+}
+
 $(document).ready(function() {
 
     $('.selectize_widget').each(function(index) {
-        var field_id = $(this).data("field_id")
-        //console.log(field_id);
-        var $select = $(this).selectize({
-            create: false,
-            sortField: 'text',
-            valueField: 'value',
-            labelField: 'text',
-            searchField: ['text'],
-            openOnFocus: true,
-            //  closeAfterSelect: true,
-
-            render: {
-                option: function (item, escape) {
-                    return '<div>' + escape(item.text) + '</div>';
-                }
-            },
-
-            load: function(query, callback) {
-                if (!query.length) return callback();
-                //url = $("#myselectize").data('url');
-                $.ajax({
-                    url: '/selectize_ajax_query/',
-                    data: { 'q': query, 'field_id': field_id },
-                    dataType: "json",
-                    type: 'GET',
-
-                    error: function() {
-                        console.log(res);
-                        callback();
-                    },
-                    success: function(res) {
-                        console.log(res);
-                        callback(res);
-                    }
-                })
-            },
-        });
-        $select[0].selectize.on( 'dropdown_close', function () {
-            $select[0].selectize.clearOptions();
-        });
+        selectize_widget_init($(this))
     })
-/*
-    clean_dropdown = function() {
-        console.log('cleaning')
-        $(this).clearOptions()
-    };
-    selectize_items = $('.selectize_widget')
-    selectize_items.on('dropdown_close', clean_dropdown);
-*/    
 })
