cb0b9ed666691058fdd7112ee9ecd2be27bd1367
konyshevn <41689868+konyshevn@users.noreply.github.com>
2019-03-28 17:31:06 +0300

    vue - build settings

--- inventory/settings.py
+++ inventory/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+APPEND_SLASH = True
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'inventory.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'static/custom/dist')],
+        'DIRS': [os.path.join(BASE_DIR, 'static/frontend/dist')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -143,7 +143,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/custom"), os.path.join(BASE_DIR, "static/custom/dist"))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/custom"), os.path.join(BASE_DIR, "static/frontend/dist"))
 
 DATETIME_INPUT_FORMATS = ['%d.%m.%Y %H:%M:%S', ]
 DATE_INPUT_FORMATS = ['%d.%m.%Y %H:%M:%S', ]
